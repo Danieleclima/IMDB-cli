575f8f21f40ec2079842941e607b5eccef22f236
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-04-25 20:34:59 +0000

    changes

--- lib/IMDB/cli/scraper.rb
+++ lib/IMDB/cli/scraper.rb
@@ -1,5 +1,12 @@
-module IMDB
-  module Cli
-    VERSION = "0.1.0"
+require 'pry'
+require 'open-uri'
+require 'nokogiri' 
+
+  def scrape_index_page (index_url)
+    doc = Nokogiri::HTML(open(index_url))
+    movies = []
+    doc.css("tbody.lister-list").each do |movie|
+      
+    
+    end
   end
-end
