2828ce64468a9e244248918eef6ea5a3df80c5e8
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-04-25 20:49:56 +0000

    changes

--- lib/IMDB/cli/scraper.rb
+++ lib/IMDB/cli/scraper.rb
@@ -2,11 +2,17 @@ require 'pry'
 require 'open-uri'
 require 'nokogiri' 
 
-  def scrape_index_page (index_url)
+  def scrape_popular_movies (index_url)
     doc = Nokogiri::HTML(open(index_url))
     movies = []
     doc.css("tbody.lister-list").each do |movie|
+      new_movie = {}
+      movie_title = doc.css("td.titleColumn a").text
+      movie_year = doc.css("span.secondaryInfo").text
       
-    
+      new_movie[:title] = movie_title
+      new_movie[:year] = movie_year 
+      movies << new_movie
     end
+    movies
   end
