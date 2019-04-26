f9f92260436c536e77bffcb74e4f05e06240611d
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-04-26 18:51:04 +0000

    changes

--- lib/IMDB/cli/scraper.rb
+++ lib/IMDB/cli/scraper.rb
@@ -10,19 +10,19 @@ class MovieScraper
   def scrape_popular_movies (index_url)
     doc = Nokogiri::HTML(open(index_url))
     movies = []
-    binding.pry
-    doc.css("tbody.lister-list td.titleColumn a").each do |movie|
+    doc.css("tbody.lister-list td.titleColumn").each do |movie|
       new_movie = {}
-      movie_title = movie.css("td.titleColumn a").text
-      movie_year = movie.css("span.secondaryInfo").text
-      
-      new_movie[:title] = movie_title
-      new_movie[:year] = movie_year 
-      movies << new_movie
+      movie_title = movie.css("a").children.text
+      movie_year = movie.css("span").children.text
+      binding.pry
+      @@movies[:title] = movie_title
+      @@movies[:year] = movie_year 
+      @@all << @@movies
     end
     movies
   end
   
 end
 
-  scrape_popular_movies("https://www.imdb.com/chart/moviemeter?ref_=nv_mv_mpm")
+ imdb = MovieScraper.new 
+ imdb.scrape_popular_movies("https://www.imdb.com/chart/moviemeter?ref_=nv_mv_mpm")
