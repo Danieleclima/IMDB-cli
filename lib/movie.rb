eac2da8c2c6c271658856f3ff012083e055bca54
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-04-27 17:48:18 +0000

    changes

--- lib/movie.rb
+++ lib/movie.rb
@@ -3,11 +3,21 @@ require 'pry'
 class Movie
   
   attr_accessor :title, :year, :url, :rating, :in_cinemas, :genre
+  @@all = []
   
   def initialize (movies_hash)
     binding.pry
-  movies_hash.each do |key, value|
-   self.send(("#{key}="), value)
+    movies_hash.each do |key, value|
+    self.send(("#{key}="), value)
+    end
+    @@all << self
   end
+  
+  def self.create_from_collection(movies_array)
+    movies_array.each do |movie|
+        Movie.new(movie)
+    end
+  end
+  
   
 end
