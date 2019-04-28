5bfe0180ecb8080fbd79c84e49df2d41ad52c57f
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-04-28 10:23:26 +0000

    changes

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -10,6 +10,7 @@ class CommandLineInterface
   def run
     create_movies
     add_attributes_to_movies
+    controller
   end
   
   def create_movies
@@ -24,7 +25,7 @@ class CommandLineInterface
     end
   end
   
-  def contoller
+  def controller
     puts "Welcome to your movie guide!"
     puts "To list the most popular movies sorted by popularity , enter 'list movies'."
     puts "To list all of the movies currently showing at the cinema, enter 'what's on'."
@@ -32,8 +33,15 @@ class CommandLineInterface
     puts "To list all of the movies for a particular genre, type the genre."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
+    answer = gets.chomp
+    list_movies if answer == 'list movies'
   end
   
+  def list_movies
+    Movie.all.each_with_index do |movie, index|
+      puts "#{index + 1}. #{movie.title} - #{movie.rating}"
+    end
+  end
   
 end
 
